189830954b28b22205c7a7879bf2995eb59d8355
Leo <Leo@Leo-NB>
2014-03-19 16:39:47 -0300

    Programa definitivo2.0

--- src/util/TablaUtil.java
+++ src/util/TablaUtil.java
@@ -8,8 +8,10 @@ package util;
 
 import java.text.SimpleDateFormat;
 import java.util.List;
+import javax.swing.DefaultCellEditor;
 import javax.swing.JOptionPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 import pojo.Concepto;
 import pojo.Empleado;
@@ -28,9 +30,15 @@ public class TablaUtil {
     
     public static void cargarModeloConcepto(DefaultTableModel modelo,List<Concepto> listaConcepto,JTable tablaOrdendelDia){
          modelo =(DefaultTableModel) tablaOrdendelDia.getModel();
-         
+         String carga = null;
          for (Concepto a : listaConcepto) {
-            Object[] filaNovedad = {a.getCodCon(),a.getDescripcion(),a.getTipo(),a.getCargaUser()}; 
+            if (a.getCargaUser()) {
+                carga = "SI";
+            }else{
+                carga = "NO";
+            }
+                
+            Object[] filaNovedad = {a.getCodCon(),a.getDescripcion(),a.getTipo(),carga}; 
             modelo.addRow(filaNovedad);
         }
          
@@ -50,24 +58,36 @@ public class TablaUtil {
     public static void cargarModeloRRHH(DefaultTableModel modelo,List<Novedad> listaNovedad,JTable tablaOrdendelDia){
          modelo =(DefaultTableModel) tablaOrdendelDia.getModel();
          for (Novedad a : listaNovedad) {
-             Object[] filaNovedad = {a.getId(),a.getFecha(),a.getEmpleado().getLegajo(),a.getEmpleado().getApellido(),a.getEmpleado().getNombre(),a.getEmpleado().getConvenio(),a.getEmpleado().getTarea(),a.getEmpleado().getSucursal().getEmpresa().getCodEmp()+"-"+a.getEmpleado().getSucursal().getEmpresa().getNombre(),a.getEmpleado().getSucursal().getCodSuc()+"-"+a.getEmpleado().getSucursal().getNombre(),a.getConcepto().getDescripcion(),a.getCantidad(),a.getObservacion()}; 
+             Object[] filaNovedad = {a.getId(),a.getFecha(),a.getEmpleado().getId(),a.getEmpleado().getLegajo(),a.getEmpleado().getApellido(),a.getEmpleado().getNombre(),a.getEmpleado().getConvenio(),a.getEmpleado().getTarea(),a.getEmpleado().getSucursal().getEmpresa().getCodEmp()+"-"+a.getEmpleado().getSucursal().getEmpresa().getNombre(),a.getEmpleado().getSucursal().getCodSuc()+"-"+a.getEmpleado().getSucursal().getNombre(),a.getConcepto().getDescripcion(),a.getCantidad(),a.getObservacion(),a.getQuien(),a.getModificador()}; 
             modelo.addRow(filaNovedad);
             
         }
         tablaOrdendelDia.getColumnModel().getColumn(0).setMaxWidth(0);
         tablaOrdendelDia.getColumnModel().getColumn(0).setMinWidth(0);
         tablaOrdendelDia.getColumnModel().getColumn(0).setPreferredWidth(0);
+        tablaOrdendelDia.getColumnModel().getColumn(2).setMaxWidth(0);
+        tablaOrdendelDia.getColumnModel().getColumn(2).setMinWidth(0);
+        tablaOrdendelDia.getColumnModel().getColumn(2).setPreferredWidth(0);
+        
     }
     
     public static void prepararTablaRRHH(DefaultTableModel modelo, JTable tablaConcepto){
-        String[] titulos = {"ID","FECHA","LEGAJO","APELLIDO","NOMBRE","CONVENIO","TAREA","EMPRESA","SUCURSAL","CONCEPTO","CANTIDAD", "OBSERVACION"};
+        String[] titulos = {"ID","FECHA","IDEMP","LEGAJO","APELLIDO","NOMBRE","CONVENIO","TAREA","EMPRESA","SUCURSAL","CONCEPTO","CANTIDAD", "OBSERVACION","USUARIO","MODIFICADOR","EDITAR"};
         modelo = new DefaultTableModel(null, titulos){
             @Override
             public boolean isCellEditable(int row, int col){
-                if(col == 9 || col == 10 || col == 11){    
+                if(col == 10 || col == 11 || col == 12 || col == 15){    
                     return true;
                 }else{
                     return false;
+                }
+            }
+            @Override
+            public Class getColumnClass(int column) {
+                if (column == 11 || column == 3) {
+                    return Integer.class;
+                } else {
+                    return Object.class;
                 }
             }
         };
@@ -75,17 +95,13 @@ public class TablaUtil {
     }
     
     public static void prepararTablaNovedades(DefaultTableModel modelo, JTable tablaNovedades, String tipo){
-        System.out.println("Tipo: "+tipo);
         final String aux = tipo;
-        System.out.println("aux: "+aux);
-        String[] titulos = {"ID","LEGAJO","APELLIDO","NOMBRE","CONCEPTO","CANTIDAD","OBSERVACION"};
-        
+        String[] titulos = {"ID","IDEMP","LEGAJO","APELLIDO","NOMBRE","CONCEPTO","CANTIDAD","OBSERVACION"};
         modelo = new DefaultTableModel(null, titulos){
             @Override
             public boolean isCellEditable(int row,int col){
                 boolean b = false;
-                System.out.println("col: "+col);
-                if(col == 4 || col == 5 || col == 6){
+                if(col == 5 || col == 6 || col == 7){
                     b = true;
                 }
                 return b;
@@ -94,23 +110,17 @@ public class TablaUtil {
         tablaNovedades.setModel(modelo);
     }
     
-    public static void prepararTablaNovedades(DefaultTableModel modelo, JTable tablaNovedades){
-        String[] titulos = {"ID","LEGAJO","APELLIDO","NOMBRE","CONCEPTO","CANTIDAD","OBSERVACION"};
-        
+    public static void prepararTablaCargaRRHH(DefaultTableModel modelo, JTable tablaNovedades){
+        String[] titulos = {"ID","IDEMP","LEGAJO","APELLIDO","NOMBRE","CONCEPTO","CANTIDAD","OBSERVACION"};
         modelo = new DefaultTableModel(null, titulos){
             @Override
             public boolean isCellEditable(int row,int col){
                 boolean b = false;
-                if(col == 4){
+                if(col == 5 || col == 6 || col == 7){
                         b = true;
-                    }else if (col == 5){
-                        b= true;
-                    }else if(col == 6){
-                        b = true;
-                    }
+                }
                 return b;
             }
-                
         };
         tablaNovedades.setModel(modelo);
     }
@@ -119,39 +129,52 @@ public class TablaUtil {
         modelo = (DefaultTableModel) tablaNovedades.getModel();
         
         for (Empleado n : listaEmpleado){
-            Object[] filaAsistencia = {"",n.getLegajo(),n.getApellido(),n.getNombre(),"Sin Novedad"}; 
+            Object[] filaAsistencia = {"",n.getId(),n.getLegajo(),n.getApellido(),n.getNombre(),"Sin Novedad"}; 
             modelo.addRow(filaAsistencia);
         }
         
         tablaNovedades.getColumnModel().getColumn(0).setMaxWidth(0);
         tablaNovedades.getColumnModel().getColumn(0).setMinWidth(0);
         tablaNovedades.getColumnModel().getColumn(0).setPreferredWidth(0);
-//        Conexion.getSessionFactory().close();
+        tablaNovedades.getColumnModel().getColumn(1).setMaxWidth(0);
+        tablaNovedades.getColumnModel().getColumn(1).setMinWidth(0);
+        tablaNovedades.getColumnModel().getColumn(1).setPreferredWidth(0);
+////        Conexion.getSessionFactory().close();
     }
     
     public static void cargarNovedadesCompleta(DefaultTableModel modelo, List<Novedad> listaEmpleado, JTable tablaNovedades){
         modelo = (DefaultTableModel) tablaNovedades.getModel();
         
         for (Novedad n : listaEmpleado){
-            Object[] filaAsistencia = {n.getId(),n.getEmpleado().getLegajo(),n.getEmpleado().getApellido(),n.getEmpleado().getNombre(),n.getConcepto().getDescripcion(),n.getCantidad(),n.getObservacion()};//,n.getEmpleado().getNombre(),n.getConcepto().getDescripcion(),n.getCantidad(),n.getObservacion()}; 
+            Object[] filaAsistencia = {n.getId(),"",n.getEmpleado().getLegajo(),n.getEmpleado().getApellido(),n.getEmpleado().getNombre(),n.getConcepto().getDescripcion(),n.getCantidad(),n.getObservacion()};//,n.getEmpleado().getNombre(),n.getConcepto().getDescripcion(),n.getCantidad(),n.getObservacion()}; 
             modelo.addRow(filaAsistencia);
         }
         
         tablaNovedades.getColumnModel().getColumn(0).setMaxWidth(0);
         tablaNovedades.getColumnModel().getColumn(0).setMinWidth(0);
         tablaNovedades.getColumnModel().getColumn(0).setPreferredWidth(0);
+        tablaNovedades.getColumnModel().getColumn(1).setMaxWidth(0);
+        tablaNovedades.getColumnModel().getColumn(1).setMinWidth(0);
+        tablaNovedades.getColumnModel().getColumn(1).setPreferredWidth(0);
     }
     
     
-    
-    
     public static void prepararTablaEmpleado(DefaultTableModel modelo, JTable tablaEmpleado){
-        String[] titulos = {"LEGAJO","APELLIDO","NOMBRE","EMPRESA","SUCURSAL","CUIT","CONVENIO","TAREA"};
+        String[] titulos = {"IDEMP","LEGAJO","APELLIDO","NOMBRE","EMPRESA","SUCURSAL","CUIT","CONVENIO","TAREA"};
         modelo= new DefaultTableModel(null,titulos){
             @Override// impleamento este metodo para que la tabla sea no editable
             public boolean isCellEditable(int row, int column) {
                 return false;
             }
+            @Override
+            public Class getColumnClass(int column) {
+                if (column == 1) {
+                    return Integer.class;
+                } else {
+                    return Object.class;
+                }
+            }
+            
         };
         tablaEmpleado.setModel(modelo);
     }  
@@ -162,8 +185,11 @@ public class TablaUtil {
     if (listaEmpleado == null)
         JOptionPane.showMessageDialog(tablaEmpleado, "LA LISTA ESTA VACIA, CARGUE UN EMPLEADO");
         for ( Empleado a : listaEmpleado) {
-            Object[] filaEmpleado = {a.getLegajo(),a.getApellido(),a.getNombre(),a.getNomSuc(),a.getNomEmp(),a.getCuit(),a.getConvenio(),a.getTarea()}; 
+            Object[] filaEmpleado = {a.getId(),a.getLegajo(),a.getApellido(),a.getNombre(),a.getNomEmp(),a.getNomSuc(),a.getCuit(),a.getConvenio(),a.getTarea()}; 
             modelo.addRow(filaEmpleado);
+            tablaEmpleado.getColumnModel().getColumn(0).setMaxWidth(0);
+            tablaEmpleado.getColumnModel().getColumn(0).setMinWidth(0);
+            tablaEmpleado.getColumnModel().getColumn(0).setPreferredWidth(0);
         }
     }
   
@@ -192,7 +218,7 @@ public class TablaUtil {
   
     public static void prepararTablaUsuario(DefaultTableModel modelo, JTable tablaUsuario){
         
-        String[] titulos = {"LEGAJO","USUARIO","DESCRIPCION","CLAVE","TIPO"};
+        String[] titulos = {"ID","LEGAJO","USUARIO","DESCRIPCION","TIPO"};
        modelo= new DefaultTableModel(null,titulos){
                 	@Override// impleamento este metodo para que la tabla sea no editable
 			public boolean isCellEditable(int row, int column) {
@@ -205,16 +231,42 @@ public class TablaUtil {
  public static void cargarModeloUsuario(DefaultTableModel modelo, List<Usuario> listaUsuario, JTable tablaUsuario){
     modelo =(DefaultTableModel) tablaUsuario.getModel();
             for (Usuario u : listaUsuario) {
-                Object[] filaUsuario = {u.getEmpleado().getLegajo(),u.getUsuario(),u.getDescripcion(),u.getClave(),u.getTipo()};
+                Object[] filaUsuario = {u.getEmpleado().getId(),u.getLegajo(),u.getUsuario(),u.getDescripcion(),u.getTipo()};
                 modelo.addRow(filaUsuario);
                // hago que la columna 0 no sea visible
-//                tablaUsuario.getColumnModel().getColumn(0).setMaxWidth(0);
-//                tablaUsuario.getColumnModel().getColumn(0).setMinWidth(0);
-//                tablaUsuario.getColumnModel().getColumn(0).setPreferredWidth(0);
+                tablaUsuario.getColumnModel().getColumn(0).setMaxWidth(0);
+                tablaUsuario.getColumnModel().getColumn(0).setMinWidth(0);
+                tablaUsuario.getColumnModel().getColumn(0).setPreferredWidth(0);
 
            }
         
  }
+ 
+ 
+ 
+ 
+ 
+ 
+ public DefaultCellEditor integerEditor;
+ StringOnlyDigitField doc = new StringOnlyDigitField();
+
+    public void StringDigitalCellEditor(){
+         //JTextField al cual le pongamos el document.
+         final JTextField integerField = new JTextField();
+         //Poniendole el document al JTextField que creamos.
+         integerField.setDocument(doc);
+         integerField.setHorizontalAlignment(JTextField.LEFT);
+
+        // insertando el JTextField  en el CellEditor.
+         integerEditor = new DefaultCellEditor(integerField) {
+             @Override
+             //metodo que se encarga de devolver el valor editado.
+             public Object getCellEditorValue() {
+                 return integerField.getText();
+             }
+         };
+
+    }
   
 }
     
